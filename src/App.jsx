941a47a44b2ca279e394fe061a1b1375bf7d1fc3
rionkhan-codes <rion.codes@gmail.com>
2025-09-24 19:30:50 +0600

    shuru matro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
 import './App.css'
+import { LayoutOne } from './layout/LayoutOne'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
 
 function App() {
-
+const myRoute = createBrowserRouter(createRoutesFromElements(
+  <Route>
+    <Route path='/' element ={<LayoutOne/>}>
+    <Route index element = {<Home/>}/>
+    <Route path='/about' element = {<About/>}/>
+    </Route>
+  </Route>
+))
   return (
    <>
-   
+   <RouterProvider router={myRoute}/>
    </>
   )
 }
